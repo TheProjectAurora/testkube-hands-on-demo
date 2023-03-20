32fa389f588cdf3ab99b3f37eb0e6eb40008520a
Sami Ahonen <sami.ahonen@bastu.net>
2023-03-20 21:46:08 +0200

    Updates and testing new version

--- tests/foass-ui-tests.js
+++ tests/foass-ui-tests.js
@@ -1,6 +1,6 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('http://foass-app.foass.svc.cluster.local') 
+    cy.visit('http://10.244.0.120') 
 
     expect(Cypress.env('testparam')).to.equal('testvalue')
 
