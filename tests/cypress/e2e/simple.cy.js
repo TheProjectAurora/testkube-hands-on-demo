e6e54b866fb8721704bcc16d4dfeab796f9d7b07
Sami Ahonen <sami.ahonen@bastu.net>
2023-03-22 17:12:09 +0200

    Cypress fixes

--- tests/cypress/e2e/simple.cy.js
+++ tests/cypress/e2e/simple.cy.js
@@ -1,9 +1,7 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('https://testkube.kubeshop.io');
-
-    cy.contains(
-      'Testkube provides a Kubernetes-native framework for test definition, execution and results'
-    );
-  });
-});
+    cy.visit('http://foass-service.foass.svc.cluster.local') 
+ 
+    cy.contains('Fuck Off As A Service')
+  })
+})
